cba529cf71f57468996204ffac7fafa6da411268
Peter Bauer <peter.bauer@bajupa.com>
2016-03-11 23:55:56 +0100

    Formatting of output, refactored.

--- DateInserter/InsertDateHandler.cs
+++ DateInserter/InsertDateHandler.cs
@@ -6,51 +6,44 @@ using MonoDevelop.Projects;
 using Mono.TextEditor;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 
 namespace DateInserter
 {
     public class InsertDateHandler : CommandHandler
     {
         private const string lINES_COUNT_DOCUMENT_NAME = "Lines Count Statistics";
-
         private TextEditorData textEditorData;
 
         protected override void Update(CommandInfo info)
         {
-            info.Enabled = true; //openDocuments != null && openDocuments.GetContent<ITextEditorDataProvider>() != null;
+            info.Enabled = GetSelectedItem() != null;
         }
 
         protected override void Run()
         {
-
-            Document linesCountDocument = IdeApp.Workbench.GetDocument(lINES_COUNT_DOCUMENT_NAME);
-            if (linesCountDocument == null)
-                linesCountDocument = IdeApp.Workbench.NewDocument(lINES_COUNT_DOCUMENT_NAME, "text/plain", "");
-            
-//            Document linesCountDocument = IdeApp.Workbench.ActiveDocument;
-            textEditorData = linesCountDocument.GetContent<ITextEditorDataProvider>().GetTextEditorData();
-
-            object selectedItem = IdeApp.ProjectOperations.CurrentSelectedItem;
+            PrepareLinesCountDocument();
+            object selectedItem = GetSelectedItem();
             if (selectedItem == null)
                 WriteEvathing();
             else
-            {
-                Solution selectedSolution = selectedItem as Solution;
-                if (selectedSolution != null)
-                    ShowSolution(selectedSolution);
-                
-                Project selectedProject = selectedItem as Project;
-                if (selectedProject != null)
-                    ShowProject(selectedProject);
-
-                ProjectFile selectedFile = selectedItem as ProjectFile;
-                if (selectedFile != null)
-                    ShowProjectFile(selectedFile);
-            }
-
+                WriteInfoOfSelectedItem(selectedItem);
         }
 
-        void WriteEvathing()
+        void PrepareLinesCountDocument()
+        {
+            Document linesCountDocument = IdeApp.Workbench.GetDocument(lINES_COUNT_DOCUMENT_NAME);
+            if (linesCountDocument == null)
+                linesCountDocument = IdeApp.Workbench.NewDocument(lINES_COUNT_DOCUMENT_NAME, "text/plain", "");
+            textEditorData = linesCountDocument.GetContent<ITextEditorDataProvider>().GetTextEditorData();
+        }
+
+        static object GetSelectedItem()
+        {
+            return IdeApp.ProjectOperations.CurrentSelectedItem;
+        }
+
+        private void WriteEvathing()
         {
             Write("ApplicationRootPath: " + FileService.ApplicationRootPath);
             ReadOnlyCollection<Solution> solutions = IdeApp.Workspace.GetAllSolutions();
@@ -75,9 +68,7 @@ namespace DateInserter
                 if (project != null)
                     ShowProject(project);
                 else
-                {
                     Write(si.Name);
-                }
             }
         }
 
@@ -91,13 +82,34 @@ namespace DateInserter
             }
         }
 
-        void ShowProjectFile(ProjectFile projectFile)
+        private void ShowProjectFile(ProjectFile projectFile)
         {
             if (projectFile != null)
             {
                 if (projectFile.Subtype == Subtype.Code && projectFile.FilePath.ToString().Trim().EndsWith(".cs"))
-                    Write("   " + projectFile.FilePath);
+                    WriteFileInfo(projectFile.FilePath);
             }
+        }
+
+        private void WriteFileInfo(FilePath filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath.ToString());
+            string fittingPath = filePath.ToString().Length > 80 ? filePath.ToString().Substring(0, 79) : filePath.ToString();
+            string info = String.Format("{0, -80} {1, 3}", fittingPath, lines.Length);
+            Write("   " + info);
+        }
+
+        private void WriteInfoOfSelectedItem(object selectedItem)
+        {
+            Solution selectedSolution = selectedItem as Solution;
+            if (selectedSolution != null)
+                ShowSolution(selectedSolution);
+            Project selectedProject = selectedItem as Project;
+            if (selectedProject != null)
+                ShowProject(selectedProject);
+            ProjectFile selectedFile = selectedItem as ProjectFile;
+            if (selectedFile != null)
+                ShowProjectFile(selectedFile);
         }
     }
 }
